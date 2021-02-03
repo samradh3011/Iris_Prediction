854de7103eda718b941941b87de640546dd6dd6e
Samradh Bhardwaj <77258318+samradh3011@users.noreply.github.com>
2021-02-03 23:43:22 +0530

    Update iris_prediction.py

--- iris_prediction.py
+++ iris_prediction.py
@@ -5,16 +5,16 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 
-st.set_page_config(page_title='Iris Prediction', page_icon='https://lh3.googleusercontent.com/proxy/MLJ6TbXxlz7Bc9CQ7ND3AMWDs6gfQb72iCRX5IG-1ObhDWLOcdrKgVmezx2vBszGHMuFfYTwK7JE_XdWuZdZiC0LfVo8JA0')
+st.set_page_config(page_title='Iris Prediction',
+                   page_icon='https://lh3.googleusercontent.com/proxy/MLJ6TbXxlz7Bc9CQ7ND3AMWDs6gfQb72iCRX5IG-1ObhDWLOcdrKgVmezx2vBszGHMuFfYTwK7JE_XdWuZdZiC0LfVo8JA0')
 st.write('''
 # Predict ***IRIS*** Flower Species
 ''')
 data = datasets.load_iris()
-data = data.drop('Id', axis=1)
 train_x = data.data
 train_y = data.target
-#------------------------------------------------------
-model = LogisticRegression(multi_class = "multinomial", solver = "newton-cg").fit(train_x, train_y)
+# ------------------------------------------------------
+model = LogisticRegression(multi_class="multinomial", solver="newton-cg").fit(train_x, train_y)
 input_data = pd.DataFrame()
 s_length = st.sidebar.slider('Sepal Length (Centimeters)', min_value=1.00, max_value=8.00)
 s_width = st.sidebar.slider('Sepal Width (Centimeters)', min_value=1.00, max_value=8.00)
@@ -24,9 +24,15 @@ input_data['Sepal Length'] = [s_length]
 input_data['Sepal Width'] = [s_width]
 input_data['Petal Length'] = [p_length]
 input_data['Petal Width'] = [p_width]
-#------------------------------------------------------
+# ------------------------------------------------------
 prediction = model.predict(input_data)
 st.header('***Input Data***')
 st.dataframe(input_data)
-st.header('***Prediction***')
-st.text(prediction)
+st.header('***Predicted Flower Species***')
+if prediction == 0:
+    prediction = 'Setosa'
+elif prediction == 1:
+    prediction = 'Versicolor'
+elif prediction == 2:
+    prediction = 'Virginica'
+st.write(f'***{prediction}***')
