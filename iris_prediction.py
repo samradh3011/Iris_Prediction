fc7463a2375f4dcb31e070a69ee23fb9eaffdb9b
Samradh Bhardwaj <77258318+samradh3011@users.noreply.github.com>
2021-02-03 23:26:08 +0530

    Update iris_prediction.py

--- iris_prediction.py
+++ iris_prediction.py
@@ -3,16 +3,16 @@ import pandas as pd
 import streamlit as st
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
-
+from sklearn import datasets
 
 st.set_page_config(page_title='Iris Prediction', page_icon='https://lh3.googleusercontent.com/proxy/MLJ6TbXxlz7Bc9CQ7ND3AMWDs6gfQb72iCRX5IG-1ObhDWLOcdrKgVmezx2vBszGHMuFfYTwK7JE_XdWuZdZiC0LfVo8JA0')
 st.write('''
 # Predict ***IRIS*** Flower Species
 ''')
-data = pd.read_csv('C:\\Users\\HP DB0186AU\\Desktop\\Data_Science\\practice\\Iris.csv')
+data = datasets.load_iris()
 data = data.drop('Id', axis=1)
-train_x = data.iloc[:, data.columns!='Species']
-train_y = data['Species']
+train_x = data.data
+train_y = data.target
 #------------------------------------------------------
 model = LogisticRegression(multi_class = "multinomial", solver = "newton-cg").fit(train_x, train_y)
 input_data = pd.DataFrame()
